4fdda213a391f6a9abe73e34cf8aff46d814337b
Ahlat85 <ahlatthobai85@gmail.com>
2021-08-27 18:34:15 +0700

    Commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,41 @@
 const agua85 = {
   init: () => {
-    const head = document.getElementsByTagName("head")[0];
-    const body = document.getElementsByTagName("body")[0];
-    
     const styles = [
-      "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+      ["https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css", "bootstrap"]
     ];
     const scripts = [
-      "https://code.jquery.com/jquery-3.6.0.js"
+      ["https://code.jquery.com/jquery-3.6.0.js", "jquery"]
     ];
     
-    styles.forEach(e => {
-      e = agua85.str_to_html(`<link rel="stylesheet" href="${e}">`);
-      head.prepend(e);
-    });
+    styles.forEach(e => agua85.css.load(e[1], e[0]));
     
-    scripts.forEach(e => {
-      e = agua85.str_to_html(`<script src=${e}"></script>`);
-      body.prepend(e);
-    });
+    scripts.forEach(e => agua85.js.load(e[1], e[0]));
+  },
+  css: {
+    load: (name, src) => {
+      const data = agua85.str_to_html(`<link style-name="${name}" rel="stylesheet" href="${src}">`);
+      document.getElementsByTagName("head")[0].append(data);
+    },
+    get: (name=undefined) => {
+      if (name) {
+        return $(`link[style-name=${name}]`);
+      } else {
+        return $(`link`);
+      }
+    }
+  },
+  js: {
+    load: (name, src) => {
+      const data = agua85.str_to_html(`<script script-name="${name}" src=${src}"></script>`);
+      document.getElementsByTagName("body")[0].append(data);
+    },
+    get: (name=undefined) => {
+      if (name) {
+        return $(`script[script-name=${name}]`);
+      } else {
+        return $(`script`);
+      }
+    }
   },
   str_to_html: str => {
     const div = document.createElement("div");
@@ -52,3 +69,5 @@ const agua85 = {
     });
   }
 }
+
+agua85.init();
